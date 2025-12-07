211ab0c2ff9f43441a456a92f427f73e7fa7759f
ouankou <anjia@ouankou.com>
2025-12-07 03:41:57 +0000

    Fix Issue 83: Link template specialization definitions and add regression test
    This commit addresses the disconnected AST issue for explicit template specializations with separate forward declarations and definitions.
    
    - Modify clang-frontend-decl.cpp to reuse existing forward declarations.
    
    - Add test2025_issue83_redeclarations.cpp to verify AST connectivity.
    
    - Update Cxx_Testcodes.cmake.
    
    Note: Unparser known limitation with double global qualification (::::) recorded in Issue 96.

--- tests/nonsmoke/functional/CompileTests/Cxx_tests/Cxx_Testcodes.cmake
+++ tests/nonsmoke/functional/CompileTests/Cxx_tests/Cxx_Testcodes.cmake
@@ -799,6 +799,7 @@ set(EXAMPLE_TESTCODES_REQUIRED_TO_PASS
   test2011_30.C
   test2011_31.C
   test2025_issue83.cpp
+  test2025_issue83_redeclarations.cpp
   test2011_32.C
   test2011_33.C
   test2011_34.C
