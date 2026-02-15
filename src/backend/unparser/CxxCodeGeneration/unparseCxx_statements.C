2dcc20cf8cf5974b9e4f56f7d1b7498e5a9347a9
ouankou <anjia@ouankou.com>
2026-02-15 05:52:30 +0000

    Revert "openmp: restore reference baselines and fix pragma handling"
    This reverts commit 87fca021b5f7ca9e6c88fac0140760d344f0b42d.

--- src/backend/unparser/CxxCodeGeneration/unparseCxx_statements.C
+++ src/backend/unparser/CxxCodeGeneration/unparseCxx_statements.C
@@ -8455,11 +8455,6 @@ void Unparse_ExprStmt::unparseOmpForSimdStatement(SgStatement *stmt,
 void Unparse_ExprStmt::unparseOmpBeginDirectiveClauses(SgStatement *stmt,
                                                        SgUnparse_Info &info) {
   ASSERT_not_null(stmt);
-  if (isSgOmpRequiresStatement(stmt) != nullptr ||
-      isSgOmpTaskwaitStatement(stmt) != nullptr) {
-    return;
-  }
-
   // optional clauses
   SgOmpClauseBodyStatement *bodystmt = isSgOmpClauseBodyStatement(stmt);
   SgOmpDeclareSimdStatement *simdstmt = isSgOmpDeclareSimdStatement(stmt);
