9625bd3224dede9d47121fa9b0d7ff765bd02920
ouankou <anjia@ouankou.com>
2026-02-28 15:31:36 -0800

    Fix invalid C++11 specimen syntax in Cxx11 tests

--- tests/nonsmoke/functional/CompileTests/Cxx11_tests/test2019_56.C
+++ tests/nonsmoke/functional/CompileTests/Cxx11_tests/test2019_56.C
@@ -1,31 +1,25 @@
 
-class A
-   {
-     public:
-          virtual int foobar();
-          int i;
-     public:
-          int foo();
-          A();
-   };
+class A {
+public:
+  virtual int foobar();
+  int i;
 
+public:
+  int foo();
+  A();
+};
 
-class B : public A
-   {
-     public:
-       // int abc;
-       // This is an old style using-declaration.
-       // BUG: This is unparsed without the public access level.
-          A::foo;
-          B();
-     private:
-          int foobar();
-   };
+class B : public A {
+public:
+  // C++11 requires a using-declaration.
+  using A::foo;
+  B();
 
+private:
+  int foobar();
+};
 
-void myFunction()
-   {
-     B o;
-     o.foo();
-   }
-
+void myFunction() {
+  B o;
+  o.foo();
+}
