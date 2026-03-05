33d28d35f9b6dc49af570d52c8c62e9507ec07b0
ouankou <anjia@ouankou.com>
2026-03-05 01:09:16 +0000

    Fix issue #305: legalize FP_CONTRACT tests and re-enable in C_tests

--- tests/nonsmoke/functional/CompileTests/C_tests/test2018_08.c
+++ tests/nonsmoke/functional/CompileTests/C_tests/test2018_08.c
@@ -1,14 +1,12 @@
 
-void foobar()
-   {
+void foobar() {
 #pragma STDC FENV_ACCESS ON
-     int n = 0;
-
 #pragma STDC FP_CONTRACT ON
 #pragma STDC FP_CONTRACT OFF
-     int x = 0;
-
+  int n = 0;
+  int x = 0;
+  {
 #pragma STDC FP_CONTRACT OFF
-     int y = 0;
-
-   }
+    int y = 0;
+  }
+}
