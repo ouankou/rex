fb867d60998440a70c66668e238c84a3018eb670
ouankou <anjia@ouankou.com>
2026-03-11 00:15:36 +0000

    Fix Cxx17 test2018_52 for standard aligned new

--- tests/nonsmoke/functional/CompileTests/Cxx17_tests/test2018_52.C
+++ tests/nonsmoke/functional/CompileTests/Cxx17_tests/test2018_52.C
@@ -1,58 +1,25 @@
-// Dynamic memory allocation for over-aligned data
+// Dynamic memory allocation for over-aligned data.
 
+#include <cstddef>
 #include <new>
 
-// This is how Intel supported this feature.
-// #include <aligned_new>
+static_assert(std::hardware_destructive_interference_size > 0);
+static_assert(std::hardware_constructive_interference_size > 0);
 
-    namespace std {
-        class bad_alloc;
-        class bad_array_new_length;
-        enum class align_val_t: size_t {};
-     // struct nothrow_t {};
-        extern const nothrow_t nothrow;
-        typedef void (*new_handler)();
-        new_handler get_new_handler() noexcept;
-        new_handler set_new_handler(new_handler new_p) noexcept;
+struct alignas(64) over_aligned_data {
+  int value;
+};
 
-     // 18.6.4, hardware interference size
-     // static constexpr size_t hardware_destructive_interference_size  = implementation-defined;
-     // static constexpr size_t hardware_constructive_interference_size = implementation-defined;
+constexpr std::size_t destructive_size =
+    std::hardware_destructive_interference_size;
+constexpr std::size_t constructive_size =
+    std::hardware_constructive_interference_size;
 
-     // DQ (1/2/2019): Make up values for implementation-defined behavior.
-        static constexpr size_t hardware_destructive_interference_size  = 64;
-        static constexpr size_t hardware_constructive_interference_size = 64;
-    };
+void *allocate_over_aligned_data() {
+  return ::operator new(sizeof(over_aligned_data),
+                        std::align_val_t{alignof(over_aligned_data)});
+}
 
-    void* operator new(std::size_t size);
-    void* operator new(std::size_t size, std::align_val_t alignment);
-    void* operator new(std::size_t size, const std::nothrow_t&) noexcept;
-    void* operator new(std::size_t size, std::align_val_t alignment,
-    			const std::nothrow_t&) noexcept;
-    void operator delete(void* ptr) noexcept;
-    void operator delete(void* ptr, std::size_t size) noexcept;
-    void operator delete(void* ptr, std::align_val_t alignment) noexcept;
-    void operator delete(void* ptr, std::size_t size,
-    			std::align_val_t alignment) noexcept;
-    void operator delete(void* ptr, const std::nothrow_t&) noexcept;
-    void operator delete(void* ptr, std::align_val_t alignment,
-    			const std::nothrow_t&) noexcept;
-    void* operator new[](std::size_t size);
-    void* operator new[](std::size_t size, std::align_val_t alignment);
-    void* operator new[](std::size_t size, const std::nothrow_t&) noexcept;
-    void* operator new[](std::size_t size, std::align_val_t alignment,
-    			const std::nothrow_t&) noexcept;
-    void operator delete[](void* ptr) noexcept;
-    void operator delete[](void* ptr, std::size_t size) noexcept;
-    void operator delete[](void* ptr, std::align_val_t alignment) noexcept;
-    void operator delete[](void* ptr, std::size_t size,
-    			std::align_val_t alignment) noexcept;
-    void operator delete[](void* ptr, const std::nothrow_t&) noexcept;
-    void operator delete[](void* ptr, std::align_val_t alignment,
-    			const std::nothrow_t&) noexcept;
-
-    void* operator new  (std::size_t size, void* ptr) noexcept;
-    void* operator new[](std::size_t size, void* ptr) noexcept;
-    void operator delete  (void* ptr, void*) noexcept;
-    void operator delete[](void* ptr, void*) noexcept;
-
+void deallocate_over_aligned_data(void *ptr) noexcept {
+  ::operator delete(ptr, std::align_val_t{alignof(over_aligned_data)});
+}
