82dffd6f9e5b20300c37c0fde12fb600d4331513
ouankou <anjia@ouankou.com>
2026-03-11 03:06:36 +0000

    Avoid unused warnings in Cxx17 test2018_52

--- tests/nonsmoke/functional/CompileTests/Cxx17_tests/test2018_52.C
+++ tests/nonsmoke/functional/CompileTests/Cxx17_tests/test2018_52.C
@@ -10,17 +10,12 @@ struct alignas(64) over_aligned_data {
   int value;
 };
 
-constexpr std::size_t destructive_size =
-    std::hardware_destructive_interference_size;
-constexpr std::size_t constructive_size =
-    std::hardware_constructive_interference_size;
-
-void *allocate_over_aligned_data() {
+[[maybe_unused]] void *allocate_over_aligned_data() {
   return ::operator new(sizeof(over_aligned_data),
                         std::align_val_t{alignof(over_aligned_data)});
 }
 
-void deallocate_over_aligned_data(void *ptr) noexcept {
+[[maybe_unused]] void deallocate_over_aligned_data(void *ptr) noexcept {
   ::operator delete(ptr, sizeof(over_aligned_data),
                     std::align_val_t{alignof(over_aligned_data)});
 }
