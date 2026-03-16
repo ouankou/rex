acc0d0fb2aab746687831cb93762081c1ee3121c
ouankou <anjia@ouankou.com>
2026-03-16 06:07:20 +0000

    Share JSProto test table for issue 447

--- tests/nonsmoke/functional/CompileTests/Cxx_tests/test2007_114.C
+++ tests/nonsmoke/functional/CompileTests/Cxx_tests/test2007_114.C
@@ -1,4 +1,5 @@
 #include "test2007_114.h"
+#include "test2007_11x_jsproto_table.h"
 
 #define JS_EXPORT_API(__type) __attribute__((visibility ("default")))  __type
 
@@ -59,41 +60,9 @@ typedef enum JSType {
 /* Dense index into cached prototypes and class atoms for standard objects. */
 typedef enum JSProtoKey {
 #define JS_PROTO(name, code, init) JSProto_##name = code,
-  // clang-format off
-  JS_PROTO(Null,                   0,     js_InitNullClass)
-  JS_PROTO(Object,                 1,     js_InitFunctionAndObjectClasses)
-  JS_PROTO(Function,               2,     js_InitFunctionAndObjectClasses)
-  JS_PROTO(Array,                  3,     js_InitArrayClass)
-  JS_PROTO(Boolean,                4,     js_InitBooleanClass)
-  JS_PROTO(Call,                   5,     js_InitCallClass)
-  JS_PROTO(Date,                   6,     js_InitDateClass)
-  JS_PROTO(Math,                   7,     js_InitMathClass)
-  JS_PROTO(Number,                 8,     js_InitNumberClass)
-  JS_PROTO(String,                 9,     js_InitStringClass)
-  JS_PROTO(RegExp,                10,     js_InitRegExpClass)
-  JS_PROTO(Script,                11,     SCRIPT_INIT)
-  JS_PROTO(XML,                   12,     XML_INIT)
-  JS_PROTO(Namespace,             13,     NAMESPACE_INIT)
-  JS_PROTO(QName,                 14,     QNAME_INIT)
-  JS_PROTO(AnyName,               15,     ANYNAME_INIT)
-  JS_PROTO(AttributeName,         16,     ATTRIBUTE_INIT)
-  JS_PROTO(Error,                 17,     js_InitExceptionClasses)
-  JS_PROTO(InternalError,         18,     js_InitExceptionClasses)
-  JS_PROTO(EvalError,             19,     js_InitExceptionClasses)
-  JS_PROTO(RangeError,            20,     js_InitExceptionClasses)
-  JS_PROTO(ReferenceError,        21,     js_InitExceptionClasses)
-  JS_PROTO(SyntaxError,           22,     js_InitExceptionClasses)
-  JS_PROTO(TypeError,             23,     js_InitExceptionClasses)
-  JS_PROTO(URIError,              24,     js_InitExceptionClasses)
-  JS_PROTO(Generator,             25,     GENERATOR_INIT)
-  JS_PROTO(Iterator,              26,     js_InitIteratorClasses)
-  JS_PROTO(StopIteration,         27,     js_InitIteratorClasses)
-  JS_PROTO(UnusedProto28,         28,     js_InitNullClass)
-  JS_PROTO(File,                  29,     FILE_INIT)
-  JS_PROTO(Block,                 30,     js_InitBlockClass)
+  TEST2007_11X_FOR_EACH_JS_PROTO(JS_PROTO)
 #undef JS_PROTO
-  JSProto_LIMIT
-  // clang-format on
+      JSProto_LIMIT
 } JSProtoKey;
 
 /* JSObjectOps.checkAccess mode enumeration. */
