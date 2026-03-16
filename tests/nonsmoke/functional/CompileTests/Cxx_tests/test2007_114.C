f47035fda800caf1c8aee8a3471f287e712ebe09
ouankou <anjia@ouankou.com>
2026-03-16 05:20:49 +0000

    Fix self-contained issue 447 Cxx tests

--- tests/nonsmoke/functional/CompileTests/Cxx_tests/test2007_114.C
+++ tests/nonsmoke/functional/CompileTests/Cxx_tests/test2007_114.C
@@ -58,41 +58,54 @@ typedef enum JSType {
 
 /* Dense index into cached prototypes and class atoms for standard objects. */
 typedef enum JSProtoKey {
-#define JS_PROTO(name,code,init) JSProto_##name = code,
-JS_PROTO(Null,                   0,     js_InitNullClass)
-JS_PROTO(Object,                 1,     js_InitFunctionAndObjectClasses)
-JS_PROTO(Function,               2,     js_InitFunctionAndObjectClasses)
-JS_PROTO(Array,                  3,     js_InitArrayClass)
-JS_PROTO(Boolean,                4,     js_InitBooleanClass)
-JS_PROTO(Call,                   5,     js_InitCallClass)
-JS_PROTO(Date,                   6,     js_InitDateClass)
-JS_PROTO(Math,                   7,     js_InitMathClass)
-JS_PROTO(Number,                 8,     js_InitNumberClass)
-JS_PROTO(String,                 9,     js_InitStringClass)
-JS_PROTO(RegExp,                10,     js_InitRegExpClass)
-JS_PROTO(Script,                11,     SCRIPT_INIT)
-JS_PROTO(XML,                   12,     XML_INIT)
-JS_PROTO(Namespace,             13,     NAMESPACE_INIT)
-JS_PROTO(QName,                 14,     QNAME_INIT)
-JS_PROTO(AnyName,               15,     ANYNAME_INIT)
-JS_PROTO(AttributeName,         16,     ATTRIBUTE_INIT)
-JS_PROTO(Error,                 17,     js_InitExceptionClasses)
-JS_PROTO(InternalError,         18,     js_InitExceptionClasses)
-JS_PROTO(EvalError,             19,     js_InitExceptionClasses)
-JS_PROTO(RangeError,            20,     js_InitExceptionClasses)
-JS_PROTO(ReferenceError,        21,     js_InitExceptionClasses)
-JS_PROTO(SyntaxError,           22,     js_InitExceptionClasses)
-JS_PROTO(TypeError,             23,     js_InitExceptionClasses)
-JS_PROTO(URIError,              24,     js_InitExceptionClasses)
-JS_PROTO(Generator,             25,     GENERATOR_INIT)
-JS_PROTO(Iterator,              26,     js_InitIteratorClasses)
-JS_PROTO(StopIteration,         27,     js_InitIteratorClasses)
-JS_PROTO(UnusedProto28,         28,     js_InitNullClass)
-JS_PROTO(File,                  29,     FILE_INIT)
-JS_PROTO(Block,                 30,     js_InitBlockClass)
-
+#define JS_PROTO(name, code, init) JSProto_##name = code,
+  JS_PROTO(Null, 0, js_InitNullClass) JS_PROTO(
+      Object, 1,
+      js_InitFunctionAndObjectClasses) JS_PROTO(Function, 2,
+                                                js_InitFunctionAndObjectClasses)
+      JS_PROTO(Array, 3, js_InitArrayClass) JS_PROTO(
+          Boolean, 4, js_InitBooleanClass) JS_PROTO(Call, 5, js_InitCallClass)
+          JS_PROTO(Date, 6, js_InitDateClass) JS_PROTO(
+              Math, 7, js_InitMathClass) JS_PROTO(Number, 8, js_InitNumberClass)
+              JS_PROTO(String, 9, js_InitStringClass) JS_PROTO(
+                  RegExp, 10,
+                  js_InitRegExpClass) JS_PROTO(Script, 11,
+                                               SCRIPT_INIT) JS_PROTO(XML, 12,
+                                                                     XML_INIT)
+                  JS_PROTO(Namespace, 13, NAMESPACE_INIT) JS_PROTO(
+                      QName, 14,
+                      QNAME_INIT) JS_PROTO(AnyName, 15,
+                                           ANYNAME_INIT) JS_PROTO(AttributeName,
+                                                                  16,
+                                                                  ATTRIBUTE_INIT)
+                      JS_PROTO(Error, 17, js_InitExceptionClasses) JS_PROTO(
+                          InternalError, 18,
+                          js_InitExceptionClasses) JS_PROTO(EvalError, 19,
+                                                            js_InitExceptionClasses)
+                          JS_PROTO(RangeError, 20, js_InitExceptionClasses) JS_PROTO(
+                              ReferenceError, 21, js_InitExceptionClasses)
+                              JS_PROTO(SyntaxError, 22, js_InitExceptionClasses)
+                                  JS_PROTO(TypeError, 23,
+                                           js_InitExceptionClasses)
+                                      JS_PROTO(URIError, 24,
+                                               js_InitExceptionClasses)
+                                          JS_PROTO(Generator, 25,
+                                                   GENERATOR_INIT)
+                                              JS_PROTO(Iterator, 26,
+                                                       js_InitIteratorClasses)
+                                                  JS_PROTO(
+                                                      StopIteration, 27,
+                                                      js_InitIteratorClasses)
+                                                      JS_PROTO(UnusedProto28,
+                                                               28,
+                                                               js_InitNullClass)
+                                                          JS_PROTO(File, 29,
+                                                                   FILE_INIT)
+                                                              JS_PROTO(
+                                                                  Block, 30,
+                                                                  js_InitBlockClass)
 #undef JS_PROTO
-    JSProto_LIMIT
+                                                                  JSProto_LIMIT
 } JSProtoKey;
 
 /* JSObjectOps.checkAccess mode enumeration. */
@@ -475,7 +488,4 @@ void
 Clear(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 {
         JS_ClearScope(cx, ((JSObject *)((void *)((argv[0]) & ~(jsval)(1)))));
-
 }
-
-
