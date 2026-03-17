d6974eae183e5cfd9509bdba2532914c841f889d
ouankou <anjia@ouankou.com>
2026-03-17 01:35:11 +0000

    Fix rose-1962 partial specialization test

--- tests/nonsmoke/functional/CompileTests/Cxx_tests/rose-1962.C
+++ tests/nonsmoke/functional/CompileTests/Cxx_tests/rose-1962.C
@@ -1,13 +1,14 @@
-typedef __SIZE_TYPE__ 	size_t;
+typedef __SIZE_TYPE__ size_t;
 
-template<typename T, typename Y = char>
-class make_array_helper;
-        
-template<typename T, size_t N>
-class make_array_helper<T> {
+template <typename T, typename Y = char> class make_array_helper;
 
-    template<typename U>
-    struct rebind {
-        typedef make_array_helper<T[N]> other;
-    };
+// Keep N deducible from the specialization argument.
+template <typename T, size_t N> class make_array_helper<T[N]> {
+public:
+  template <typename U> struct rebind {
+    typedef make_array_helper<U[N]> other;
+  };
 };
+
+typedef make_array_helper<int[4]>::rebind<long>::other rebound_array_helper;
+rebound_array_helper make_array;
