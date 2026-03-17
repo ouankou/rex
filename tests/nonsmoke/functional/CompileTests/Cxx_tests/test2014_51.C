ce6381d02fc0b2f0e3ed646f51ee4103169a1668
ouankou <anjia@ouankou.com>
2026-03-17 07:12:18 +0000

    Fix and re-enable Cxx test2014_51

--- tests/nonsmoke/functional/CompileTests/Cxx_tests/test2014_51.C
+++ tests/nonsmoke/functional/CompileTests/Cxx_tests/test2014_51.C
@@ -1,25 +1,17 @@
-template <int I> 
-class A 
-   {
-     static char s[I+1];
+template <int I> class A {
+  static char s[I + 1];
 
-     // This demonstrates a problem in the legacy frontend/ROSE connection. It
-     // is related to a legacy frontend 4.8 specific problem that I notice in
-     // test2013_242.C (in convert_constant_template_parameter(): case
-     // tpck_expression).
+  // This demonstrates a problem in the legacy frontend/ROSE connection. It
+  // is related to a legacy frontend 4.8 specific problem that I notice in
+  // test2013_242.C (in convert_constant_template_parameter(): case
+  // tpck_expression).
 
-     // unsigned long const src_len = 7;
-     const unsigned long src_len = 7;
-  // char buf[src_len + 1];
-  // char buf[src_len];
-     char buf[src_len + 1];
-	};
+  static constexpr unsigned long src_len = 7;
+  char buf[src_len + 1];
+};
 
 // template <int I> const unsigned long A<I>::src_len = 7;
 
-template <int I> char A<I>::s[I+1] = { 0 };
+template <int I> char A<I>::s[I + 1] = {0};
 
-void foo()
-   {
-     A<2> aa;
-   }
+void foo() { A<2> aa; }
