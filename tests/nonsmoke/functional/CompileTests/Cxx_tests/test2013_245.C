9b0bbfb5e7dba081966d51ac26ab9636efa0fdc5
ouankou <anjia@ouankou.com>
2026-03-19 02:59:12 +0000

    tests: make test2013_245 self-contained

--- tests/nonsmoke/functional/CompileTests/Cxx_tests/test2013_245.C
+++ tests/nonsmoke/functional/CompileTests/Cxx_tests/test2013_245.C
@@ -1,2 +1,9 @@
-#include <QtCore>
-int main() { return 0; }
+// Keep the extensionless angle-bracket include that this specimen was meant to
+// cover without depending on external Qt headers.
+#include <Test2013_245Header>
+
+int main() {
+  test2013_245::Wrapper<int> wrapper(test2013_245::compute(41));
+  test2013_245::Wrapper<int>::value_type answer = wrapper.value;
+  return answer - 42;
+}
